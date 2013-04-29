0fc1d144ebaef97e2b0cb9f4b8c0b5c8af7c99c7
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-04-29 11:04:48 -0700

    Release 0.1.0.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,16 +7,7 @@ RSpec.configure do |config|
   config.include(RDF::Spec::Matchers)
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
-  config.exclusion_filter = {:ruby => lambda { |version|
-    RUBY_VERSION.to_s !~ /^#{version}/
-  }}
-end
-
-def fixture_path(filename)
-  File.join(File.dirname(__FILE__), 'data', filename)
-end
-
-if "".respond_to?(:encoding)
-  Encoding.default_external = Encoding::UTF_8
-  Encoding.default_internal = Encoding::UTF_8
+  config.exclusion_filter = {
+    :ruby       => lambda {|version| RUBY_VERSION.to_s !~ /^#{version}/},
+  }
 end
